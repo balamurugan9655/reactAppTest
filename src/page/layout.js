d64033639ba09244d8c1fee49deabde78560d2f7
balamurugan9655 <shanmugambala80@gmail.com>
2024-12-27 12:25:27 +0530

    Updated content

--- src/page/layout.js
+++ src/page/layout.js
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
+//import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 //import NavDropdown from 'react-bootstrap/NavDropdown';

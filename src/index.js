26549c3174a6095f850b70a9487e5180dd1864c1
balamurugan9655 <shanmugambala80@gmail.com>
2024-12-28 11:13:00 +0530

    Updated content

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 import React from "react";
 import ReactDOM from 'react-dom/client' ;
-import { HashRouter as Router, Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes , Route, useNavigate } from "react-router-dom";
 import Layout from "./page/layout";
 import Home from "./page/home";
 import Contact from "./page/contact";
 import Blog from "./page/blog";
 import NoPage from "./page/noPage";
+import { useEffect } from 'react';
+
+const RedirectHandler = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get("redirect");
+    if (redirectPath) {
+      navigate(redirectPath, { replace: true });
+    }
+  }, [navigate]);
+
+  return null;
+};
 
 export default function App() {
   return(
-    <Router>
+    <Router basename="/reactAppTest">
       <Routes>
         <Route path="/" element={<Layout />} >
+          <Route path="*" element={<RedirectHandler/>}/>
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
           <Route path="blog" element={<Blog />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="nopage" element={<NoPage />} />
         </Route>
       </Routes>
     </Router>
